92ab046de03b2ab502a5ed12ae3ed50a66137eb8
Nicolas Milliard <contact@nicolasmilliard.fr>
2026-05-03 20:37:34 +0200

    refactor: update inBio logo

---
+++ src/components/icons/InBioLogo.tsx
@@ -0,0 +1,7 @@
+export const InBioLogo = () => {
+  return (
+    <span className="bg-accent text-primary flex h-20 items-center justify-center rounded-b-lg px-3.5 py-7 text-3xl font-extrabold">
+      inBio
+    </span>
+  );
+};
