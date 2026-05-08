d0846e0c6247e64c4d0b4dfbca881bcaba67b68c
Nicolas Milliard <73180392+NicolasMilliard@users.noreply.github.com>
2026-05-08 17:25:59 +0200

    fix: smooth AppHeader blur

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -10,8 +10,17 @@ export const AppHeader = () => {
   const isHome = !!matchRoute({ to: '/' });
 
   return (
-    <header className="sticky top-0 z-50 w-full backdrop-blur">
-      <div className="mx-auto flex h-20 max-w-6xl items-center justify-between px-4">
+    <header className="sticky top-0 z-50 w-full">
+      <div
+        className="from-background/80 via-background/40 to-background/0 absolute inset-0 bg-linear-to-b backdrop-blur"
+        style={{
+          maskImage: 'linear-gradient(to bottom, black 80%, transparent 100%)',
+          WebkitMaskImage:
+            'linear-gradient(to bottom, black 80%, transparent 100%)',
+        }}
+      />
+
+      <div className="relative mx-auto flex h-20 max-w-6xl items-center justify-between px-4">
         <Link to="/">
           <InBioLogo />
         </Link>
