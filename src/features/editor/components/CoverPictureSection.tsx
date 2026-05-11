1cd931c63b9257146b0e14713a931f5c7248ea21
Nicolas Milliard <73180392+NicolasMilliard@users.noreply.github.com>
2026-05-11 17:56:31 +0200

    refactor: Simplify CoverPictureSection component structure and improve readability

--- src/features/editor/components/CoverPictureSection.tsx
+++ src/features/editor/components/CoverPictureSection.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { type MetadataFormValues } from '../schemas/metadataForm.schema';
 
@@ -5,16 +6,26 @@ export const CoverPictureSection = () => {
   const { control } = useFormContext<MetadataFormValues>();
   const coverPicture = useWatch({ control, name: 'coverPicture.preview' });
 
+  const hasCoverPicture = !!coverPicture;
+
   return (
     <>
-      {coverPicture && (
-        <div className="fixed inset-0 z-0 overflow-hidden">
-          <div
-            className="absolute inset-0 scale-110 bg-cover bg-center blur brightness-85 saturate-130"
-            style={{ backgroundImage: `url(${coverPicture})` }}
-          />
-        </div>
-      )}
+      <div className="fixed inset-0 z-0 overflow-hidden">
+        <div
+          className={cn(
+            'absolute inset-0 scale-110 bg-cover bg-center blur brightness-85 saturate-130',
+            hasCoverPicture ? 'bg-none' : 'bg-background',
+          )}
+          style={
+            hasCoverPicture
+              ? { backgroundImage: `url(${coverPicture})` }
+              : undefined
+          }
+        />
+        {!hasCoverPicture && (
+          <div className="absolute inset-0 bg-[radial-gradient(#ebeafa_1px,#fdfdfd_1px)] bg-size-[20px_20px]" />
+        )}
+      </div>
       <div className="bg-muted/18 fixed inset-0 z-1 sm:hidden" />
     </>
   );
